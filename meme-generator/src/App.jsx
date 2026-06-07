3a28489b8aca7d8a63ef8dae130e71408d503750
Shubham-Chaprana <shubham.chaprana.ug25@nsut.ac.in>
2026-06-07 22:58:39 +0530

    simple meme generator

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Main from "./components/Body"
+import "./index.css"
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
